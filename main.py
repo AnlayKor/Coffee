2e7d2bb32b8fd89d0afc5f79026cbfcca406781d
AnlayKor <150436643+AnlayKor@users.noreply.github.com>
2024-02-24 18:42:02 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
+from PyQt5.QtWidgets import QMainWindow, QWidget, QTableWidgetItem
 
 
 class DBSample(QMainWindow):
@@ -11,6 +11,7 @@ class DBSample(QMainWindow):
         super().__init__()
         uic.loadUi('main.ui', self)
         self.connection = sqlite3.connect("coffee.sqlite")
+        self.pushButton.clicked.connect(self.open)
         # По умолчанию будем выводить все данные из таблицы films
         self.select_data()
 
@@ -34,6 +35,68 @@ class DBSample(QMainWindow):
         # При закрытии формы закроем и наше соединение
         # с базой данных
         self.connection.close()
+
+    def open(self):
+        self.new_form = AddWidget()
+        self.new_form.show()
+
+
+class AddWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("addEditCoffeeForm.ui", self)
+        self.con = sqlite3.connect("coffee.sqlite")
+        self.pushButton.clicked.connect(self.update_result)
+        self.tableWidget.itemChanged.connect(self.item_changed)
+        self.pushButton_2.clicked.connect(self.save_results)
+        self.pushButton_3.clicked.connect(self.insert_results)
+        self.modified = {}
+        self.titles = None
+
+    def update_result(self):
+        cur = self.con.cursor()
+        # Получили результат запроса, который ввели в текстовое поле
+        result = cur.execute("SELECT * FROM coffee WHERE id=?",
+                             (item_id := self.spinBox.text(),)).fetchall()
+        # Заполнили размеры таблицы
+        self.tableWidget.setRowCount(len(result))
+        # Если запись не нашлась, то не будем ничего делать
+        if not result:
+            self.label_2.setText('Ничего не нашлось')
+            return
+        else:
+            self.label_2.setText(f"Нашлась запись с id = {item_id}")
+        self.tableWidget.setColumnCount(len(result[0]))
+        self.titles = [description[0] for description in cur.description]
+        # Заполнили таблицу полученными элементами
+        for i, elem in enumerate(result):
+            for j, val in enumerate(elem):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
+        self.modified = {}
+
+    def item_changed(self, item):
+        # Если значение в ячейке было изменено,
+        # то в словарь записывается пара: название поля, новое значение
+        self.modified[self.titles[item.column()]] = item.text()
+
+    def save_results(self):
+        if self.modified:
+            cur = self.con.cursor()
+            que = "UPDATE coffee SET "
+            que += ", ".join([f"{key} = '{self.modified.get(key)}'"
+                              for key in self.modified.keys()])
+            que += f" WHERE id = ?"
+            cur.execute(que, (self.spinBox.text(),))
+            self.con.commit()
+            self.modified.clear()
+
+    def insert_results(self):
+        for i in range(1, 7):
+            self.modified[self.titles[i]] = self.tableWidget.item(0, i).text()
+        cur = self.con.cursor()
+        cur.execute(f'''INSERT INTO coffee({','.join(list(self.modified))}) VALUES({','.join(list(self.modified.values()))})''')
+        self.con.commit()
+        self.modified.clear()
 
 
 if __name__ == '__main__':
